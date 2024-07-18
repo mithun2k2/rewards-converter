a14ea06139d885fe4cfce1da3b2b9a97b599da07
mithun2k2 <mithun2k2eee@gmail.com>
2024-07-18 19:36:41 +0100

    Two test features has been added

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,22 +1,21 @@
 public class RewardValue {
-    public static final double miles_to_cash_rate = 0.0035;
-    public double cashValue;
-    public double milesValue;
+    private static final double miles_to_cash_rate = 0.0035;
+    private double cashValue;
+    private int milesValue;
 
     // Constructor that accepts a cash value
 
     public RewardValue(double cashValue){
         this.cashValue = cashValue;
-        this.milesValue = cashValue / miles_to_cash_rate;
+        this.milesValue = (int) (cashValue / miles_to_cash_rate);
     }
     // Constructor that accepts a miles value
 
-    public RewardValue(double milesValue, boolean isMiles){
-        if(isMiles){
+    public RewardValue(int milesValue){
         this.milesValue = milesValue;
         cashValue = milesValue * miles_to_cash_rate;
-        }
     }
+    
 
     //Method to get the cash Value
 
